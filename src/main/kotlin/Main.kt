75886e44bb0068868018a2080e18a987e1346824
Shashwat Samadhiya <sshashwat934@gmail.com>
2023-08-01 08:29:23 +0530

    var and val

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,8 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
+fun main() {
+    var userName:String="Shashwat"
+    var age:Int?=null
+    /// type inference is process by which the type of variable
+    /// can be deducted by the value assigned
+    var number=33
+    println("Hello $userName! your age is $age")
 }
